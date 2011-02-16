c7456e2723474641cd1e471cdcaf89cdefc3e876
lann <lann@lannbox.com>
2011-02-16 11:05:51 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'oauth-client',
+    version = '0.1',
+    packages = ['oauth_client'],
+    
+    author = 'Lann Martin',
+    author_email = 'oauth-client@lannbox.com',
+    description = 'client library for OAuth 1.0',
+    license = 'MIT',
+    url = 'http://github.com/lann/python-oauth-client',
+
+    test_suite = 'tests',
+    tests_require = 'mock',
+    )
